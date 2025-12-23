86c6629f48007fae21de9afe0518246922beca50
Richard Souza <souzza.richard25@gmail.com>
2025-12-23 18:26:05 +0000

    core: Fix WSL Clipboard enconding

--- config/extra_config.lua
+++ config/extra_config.lua
@@ -1,44 +1,48 @@
+-- Allows Neovim to use language-specific indent rules
+
+vim.cmd("filetype plugin indent on")
+
 -- Enable clip and powershell for WSL
 if vim.fn.has("wsl") == 1 then
-    vim.g.clipboard = {
-        name = "WslClipboard",
-        copy = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe",
-        },
-        paste = {
-            ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
-    }
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = { "sh", "-c", "iconv -f UTF-8 -t UTF-16LE | clip.exe" },
+      ["*"] = { "sh", "-c", "iconv -f UTF-8 -t UTF-16LE | clip.exe" },
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
 end
 
 -- Ignore builtin colorschemes
 
 vim.opt.wildignore:append({
-    "blue.vim",
-    "darkblue.vim",
-    "delek.vim",
-    "desert.vim",
-    "elflord.vim",
-    "evening.vim",
-    "industry.vim",
-    "koehler.vim",
-    "lunaperche.vim",
-    "morning.vim",
-    "murphy.vim",
-    "pablo.vim",
-    "peachpuff.vim",
-    "quiet.vim",
-    "retrobox.vim",
-    "ron.vim",
-    "shine.vim",
-    "slate.vim",
-    "torte.vim",
-    "unokai.vim",
-    "vim.lua",
-    "wildcharm.vim",
-    "zaibatsu.vim",
-    "zellner.vim",
+  "blue.vim",
+  "darkblue.vim",
+  "delek.vim",
+  "desert.vim",
+  "elflord.vim",
+  "evening.vim",
+  "industry.vim",
+  "koehler.vim",
+  "lunaperche.vim",
+  "morning.vim",
+  "murphy.vim",
+  "pablo.vim",
+  "peachpuff.vim",
+  "quiet.vim",
+  "retrobox.vim",
+  "ron.vim",
+  "shine.vim",
+  "slate.vim",
+  "torte.vim",
+  "unokai.vim",
+  "vim.lua",
+  "wildcharm.vim",
+  "zaibatsu.vim",
+  "zellner.vim",
 })
