d5eb581d8ce714e1acd08a0759c436b7ecf7a632
Richard Souza <souzza.richard25@gmail.com>
2026-01-22 11:19:55 +0000

    ui: Implements methods for defining which-key icons

--- config/keymaps/_shared.nix
+++ config/keymaps/_shared.nix
@@ -1,8 +1,29 @@
 {
-  smart_jump = key: ''
+  setIcon = { key, icon, color ? "azure" }:
+    {
+      __unkeyed-1 = key;
+      icon = {
+        icon = icon;
+        color = color;
+      };
+      mode = [ "n" ];
+    };
+
+  setIconGroup = { group, key, icon, color ? "azure" }:
+    {
+      __unkeyed-1 = key;
+      group = group;
+      icon = {
+        icon = icon;
+        color = color;
+      };
+      mode = [ "n" ];
+    };
+
+  smartJump = key: ''
     function()
       local count = vim.v.count
-      if count > 5 then
+      if count >= 3 then
         vim.cmd("normal! m'")
       end
       vim.cmd("normal! " .. (count > 0 and count or 1) .. "${key}")
